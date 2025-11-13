9b724b62494d915ae47de39859ea196307f24bd9
toastoffee <931391442@qq.com>
2025-11-13 20:09:45 +0800

    hamoodhabb

--- src/dataset/dataset_utils.py
+++ src/dataset/dataset_utils.py
@@ -1,6 +1,8 @@
+import random
+from collections import defaultdict
 import numpy as np
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, Subset
 
 
 def set_seeds(seed: int):
@@ -9,7 +11,9 @@ def set_seeds(seed: int):
     :param seed: the random seed
     :return: void
     """
+    random.seed(seed)
     torch.manual_seed(seed)
+    torch.cuda.random(seed)
     np.random.seed(seed)
 
 
@@ -28,13 +32,63 @@ def dataset_random_split(dataset: Dataset,
 
 
 def random_subset(dataset: Dataset,
-                  cnt:     int,
-                  seed:    int):
+                  n:       int,
+                  seed:    int) -> Subset:
     """
     randomly pick samples from dataset and return a subset
     :param dataset: dataset to select from
-    :param cnt: the count of samples selected
+    :param n: the count of samples selected
     :param seed: random seed
     :return: subset of dataset
     """
+    if n > len(dataset):
+        raise ValueError(f"n is larger than dataset size")
+
     set_seeds(seed)
+
+    indices = random.sample(range(len(dataset)), n)
+
+    return Subset(dataset, indices)
+
+
+def shuffle_dataset(dataset: Dataset,
+                    seed:    int) -> Subset:
+    """
+    return the shuffled subset of dataset
+    :param dataset: dataset to shuffle
+    :param seed: random seed
+    :return: subset of the shuffled dataset
+    """
+    set_seeds(seed)
+
+    indices = np.random.permutation(len(dataset))
+    return Subset(dataset, indices)
+
+
+# def get_few_shots(dataset: Dataset,
+#                   shot:    int,
+#                   seed:    int) -> Subset:
+#     """
+#     get random samples, shot for each class
+#     :param dataset: dataset pick from
+#     :param shot: sample count of each class
+#     :param seed: random seed
+#     :return: subset of few shots
+#     """
+#     set_seeds(seed)
+#
+#     class_indices = defaultdict(list)
+#     for idx in range(len(dataset)):
+#         _, label, _ = dataset[idx]
+#         label = int(label)
+#         class_indices[label].append(idx)
+#
+#     selected_indices = []
+#     for cls, indices in class_indices.items():
+#         # 确保每个类别都采样shot个样本
+#         cls_selected = random.sample(indices, shot)
+#         selected_indices.extend(cls_selected)
+#
+#     assert len(selected_indices) == len(class_indices) * shot
+#
+#     return Subset(dataset, selected_indices)
--- src/dataset/fada_dataloader.py
+++ src/dataset/fada_dataloader.py
@@ -1,8 +1,7 @@
 from torch.utils.data import Dataset, DataLoader
 import torch
 
-from dataloader_helper import DataloaderHelper
-from rml_dataset import RmlHelper
+import dataset_utils
 
 
 # class FadaDataloaderHelper:
@@ -19,29 +18,6 @@ from rml_dataset import RmlHelper
 #         target_labeled_subset = FadaDataloaderHelper.get_labeled_samples(target_train_dataset, 1)
 #
 #
-#
-#     @staticmethod
-#     def get_labeled_samples(dataset: Dataset,
-#                             shot:    int):
-#         from collections import defaultdict
-#         import random
-#         from torch.utils.data import Subset
-#
-#         class_indices = defaultdict(list)
-#         for idx in range(len(dataset)):
-#             _, label, _ = dataset[idx]
-#             label = int(label)
-#             class_indices[label].append(idx)
-#
-#         selected_indices = []
-#         for cls, indices in class_indices.items():
-#             # 确保每个类别都采样shot个样本
-#             cls_selected = random.sample(indices, shot)
-#             selected_indices.extend(cls_selected)
-#
-#         assert len(selected_indices) == len(class_indices) * shot
-#
-#         return Subset(dataset, selected_indices)
 
 class FadaDataloader:
 
@@ -49,9 +25,39 @@ class FadaDataloader:
                  source_dataset: Dataset,
                  target_dataset: Dataset,
                  train_ratio:    float,
-                 ):
+                 batch_size:     int,
+                 shots:          int,
+                 seed:           int):
+        """
+        initialize the dataloader
+        :param source_dataset: dataset of source domain
+        :param target_dataset: dataset of target domain
+        :param train_ratio: train ratio of the datasets
+        :param batch_size: batch size
+        :param shots: sample count of each class
+        :param seed: random seed
+        """
+        self.shots = shots
+        self.split_ratio = [train_ratio, 1.0 - train_ratio]
+        self.batch_size = batch_size
+        self.source_train_subset, self.source_valid_subset = dataset_utils.dataset_random_split(source_dataset, self.split_ratio, seed)
+        self.target_train_subset, self.target_valid_subset = dataset_utils.dataset_random_split(target_dataset, self.split_ratio, seed)
+        self.target_labeled_subset = dataset_utils.get_few_shots(self.target_train_subset, self.shots, seed)
+
+    def create_pair_groups(self, seed: int):
+        """
+        get G1,G2,G3,G4 pairs
+        G1: a pair of pic comes from same domain ,same class
+        G3: a pair of pic comes from same domain, different classes
+        G2: a pair of pic comes from different domain,same class
+        G4: a pair of pic comes from different domain, different classes
+        :param seed: random seed
+        :return: group of pairs, and labels
+        """
+        source_labeled_subset = dataset_utils.get_few_shots(self.source_train_subset, self.shots * 2, seed)
+
 
 
 
 if __name__ == "__main__":
-    FadaDataloaderHelper.dataloader_fada(1, 0.6)
+    # FadaDataloaderHelper.dataloader_fada(1, 0.6)
